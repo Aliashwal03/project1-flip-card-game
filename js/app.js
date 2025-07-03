cee62f6f1d0975a66703df94a7e42b5dfbe82673
Ali Alashawal <aliashwal03@gmail.com>
2025-07-03 13:38:10 +0300

    added the timer and fixed some minor issues

--- js/app.js
+++ js/app.js
@@ -20,27 +20,34 @@ const cards = [
 /*---------------------------- Variables (state) ----------------------------*/
 let cardsCombo
 let win
-let attemps
+let attemps=0
 let timer
 let board
 let flippedCards = []; 
+let interval
+let matchedPairs = 0;
+
 
 
 /*------------------------ Cached Element References ------------------------*/
 const cardsEls=document.querySelectorAll('.cards')
 const messageEl=document.querySelector('#message')
 const resetBtnEl=document.querySelector('#reset')
+const timerEl=document.querySelector('#timer')
+const counEl=document.querySelector('counter')
 
 /*-------------------------------- Functions --------------------------------*/
 function init() {
     cardsCombo = cards.sort(() => Math.random() - 0.5);
     win = false;
-    timer = 30;
+    timer =10;
+    matchedPairs = 0;
     cardsEls.forEach((element, index) => {
         element.textContent = "?";           
         element.className = "cards";         
         element.style.opacity = "1";        
     });
+    countdown()
 }
 
 function flipCards(cardIndex) {
@@ -51,18 +58,22 @@ function flipCards(cardIndex) {
 
 
 function handleCardClick(index) {
-    if (flippedCards.includes(index)){
-        return;
-    } 
+    if (timer <= 0 || win) return;
+
+    if (flippedCards.includes(index)) return;
+
     flipCards(index);
     flippedCards.push(index);
 
     if (flippedCards.length === 2) {
         setTimeout(() => {
             compareFlipped();
-        }, 400) 
+        }, 400)
+        attemps=attemps+1
+        cou
     }
 }
+
 
 function compareFlipped() {
     const [firstCard, secondCard] = flippedCards;
@@ -73,13 +84,47 @@ function compareFlipped() {
         cardsEls[secondCard].className = "cards";
     }
     flippedCards = [];
+    console.log(flippedCards)
 
 }
 
+function winningLossCondition(){
+    if(timer >=0 &&flipCards.length==15){
+    win=true
+    messageEl.textContent=`congrats you did it`
+    return
+    }
+    else{
+        messageEl.textContent=`you lost`
+
+    }    
+}
+
+function countdown(){
+    interval=setInterval(()=>{
+    timerEl.textContent=timer
+    timer=timer-1
+    if (timer==-1){
+        clearInterval(interval)
+        messageEl.textContent=`Time is up u lost`
+        cardsEls.forEach((element)=>{
+            element.textContent=`?`
+            element.className='cards'
+
+        })
+    }
+    },1000)
+    
+}
 init()
 /*----------------------------- Event Listeners -----------------------------*/
-resetBtnEl.addEventListener('click',init)
+resetBtnEl.addEventListener('click',()=> {
+    clearInterval(interval)
+    console.log(clearInterval(interval))
+    init()
+})
 
 cardsEls.forEach((element, index) => {
     element.addEventListener('click', () => handleCardClick(index));
 });
+
